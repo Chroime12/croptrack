a4f8ec8302aa0f2bc020983d7a46280699fe3f75
Hanz <hanzchrisromechico@gmail.com>
2025-01-04 16:09:55 +0800

    Added a firebase to the project.

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,8 @@
+import 'package:croptrack/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class AppPages {
+  static Map<String, WidgetBuilder> routes = {
+    AppRoutes.home: (context) => 
+  };
+}
